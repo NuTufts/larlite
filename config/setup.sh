1db7b8e0fd919c0a7513a80d0d1bf875405055a9
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2021-09-05 00:33:16 -0400

    change path env var LARLITE_COREDIR points to. was causing larutil to not work

--- config/setup.sh
+++ config/setup.sh
@@ -45,7 +45,7 @@ LARLITE_OS=`uname -s`
 # Set path to sub directories
 export LARLITE_LIBDIR=$LARLITE_BASEDIR/build/installed/lib
 export LARLITE_INCDIR=$LARLITE_BASEDIR/build/installed/include
-export LARLITE_COREDIR=$LARLITE_BASEDIR/larlite/core
+export LARLITE_COREDIR=$LARLITE_BASEDIR/larlite/
 
 if [[ -z $USER_MODULE ]]; then
     export USER_MODULE=""
